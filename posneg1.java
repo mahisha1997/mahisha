3bf87a699a3adf90ecff18f6b00941b3078d58ff
mahisha1997 <42768422+mahisha1997@users.noreply.github.com>
2018-12-18 09:57:33 +0530

    Create posneg1.java

---
+++ posneg1.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone
+{
+public static void main(String[]args)
+{
+int num;
+Scanner scan = new Scanner(System.in);
+num = scan.nextInt();
+if(num>0)
+{
+System.out.println("positive");
+}
+else if(num==0)
+{
+System.out.println("Zero");
+}
+else
+{
+System.out.println("Negative");
+}
+}
+}
+

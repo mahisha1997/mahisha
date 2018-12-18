436e7a64fd8b459e88224afb5f43be25eb0c4904
mahisha1997 <42768422+mahisha1997@users.noreply.github.com>
2018-12-18 09:39:32 +0530

    Create posneg.java

---
+++ posneg.java
@@ -0,0 +1,30 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main(String[]args)
+	{      
+		int num;
+		Scanner scan = new Scanner(System.in);
+      
+        num = scan.nextInt();
+                       
+                        if(num>0)
+                        {
+		System.out.println("Positive");
+                        }
+		else if(num==0)
+		{
+		System.out.println("Zero");
+		}
+		else
+		{
+			System.out.println("Negative");
+		
+	}}
+}
